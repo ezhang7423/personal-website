3e8e544c6faeb6cc704aa1dabafd01a6afc3887a
ezhang7423 <ezhang7423@student.palomar.edu>
2021-06-19 15:03:21 -0700

    minfiy

--- minify_images.sh
+++ minify_images.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd img/real-portfolio
 sudo apt install optipng -y
+
+
 ## Keep file system permission and make a backup of original PNG (see options below)  ##
-for i in *.png; do optipng -o5 -clobber -quiet -preserve -dir . "$i"; done
+for i in *.png; do echo "Optimizing $i" && optipng -o5 -clobber -quiet -preserve -dir . "$i"; done
+
+echo "Done optimizing!"
